0aa92b29f43269f336b5eee8704fcc9c26678d48
Aaron Ecay <aaronecay@gmail.com>
2015-05-11 19:24:41 +0100

    graphs.R: new graphs

--- R/graphs.R
+++ R/graphs.R
@@ -26,3 +26,46 @@ graph.gen.n <- function (data) {
       scale_size_area() +
       geom_smooth(aes(weight = n))
 }
+
+graph.ip1.particle <- function (data) {
+    data %>%
+      group_by(text, year) %>%
+      filter(v1 != "v1", first.elt == "_", clause.type == "mat", polarity == "pos",
+             mood == "indicative", verb.type == "lexical", conj == "_")  %>%
+      summarize(infl.medial = sum(infl.particle == "infl-medial"),
+                infl.final  = sum(infl.particle == "infl-final")) %>%
+      mutate(n = infl.medial + infl.final) %>%
+      ggplot(aes(x = year, y = infl.medial / n)) +
+        geom_point(aes(size = n)) +
+        scale_size_area() +
+        geom_smooth(aes(weight = n))
+}
+
+zimm.plot <- function (data, zimm) {
+    z <- zimm %>% select(text, IP1)
+    d <- data %>%
+      group_by(text, year) %>%
+      filter(v1 == "_", first.elt == "_", clause.type == "mat", conj == "_") %>%
+      summarize(infl.medial = sum(infl.part.big == "infl-medial"),
+                infl.final  = sum(infl.part.big == "infl-final")) %>%
+      mutate(n = infl.medial + infl.final, p = infl.medial / n)
+
+    m <- inner_join(z, d)
+    ggplot(m, aes(x = p, y = IP1)) +
+      geom_point() +
+      coord_cartesian(xlim = 0:1, ylim = 0:1) +
+      geom_abline(slope = 1, intercept = 0)
+}
+
+graph.ip1.prep <- function (data) {
+    data %>%
+      group_by(text, year) %>%
+      filter(v1 != "v1", first.elt == "_", clause.type == "mat")  %>%
+      summarize(infl.medial = sum(infl.stranded.p == "infl-medial"),
+                infl.final  = sum(infl.stranded.p == "infl-final")) %>%
+      mutate(n = infl.medial + infl.final) %>%
+      ggplot(aes(x = year, y = infl.medial / n)) +
+        geom_point(aes(size = n)) +
+        scale_size_area() +
+        geom_smooth(aes(weight = n))
+}
