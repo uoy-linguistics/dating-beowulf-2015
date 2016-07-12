d17ec5ba3003e7c05e924872fec2fc00e9e76be4
Aaron Ecay <aaronecay@gmail.com>
2016-07-12 14:12:04 +0100

    fix-id.py: add explanatory comment

--- scripts/fix-ids.py
+++ scripts/fix-ids.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python2
 
+# The default output of CorpusSearch contains the coding string, with each
+# column separated by a :, followed by an @ then the ID.  For reading into R,
+# we need to make : the delimiter of all fields, including the ID.  We also
+# need to change :s in the ID (which would otherwise be interpreted as
+# delimting a spurious column) to another character, in this case a ;
+
 import sys
 
 with open(sys.argv[1]) as f:
